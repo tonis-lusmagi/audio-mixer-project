b18318260831f4b0729e1f6b0e9c4bf75b30a8b1
martin.perman <martin.perman@ttu.ee>
2018-05-11 15:13:21 +0300

    fixes

--- driver/src/uio/uiodriver_full.c
+++ driver/src/uio/uiodriver_full.c
@@ -342,11 +342,9 @@ void *send_audio_function(void *arg)
 	while (1)
 	{
 		read(fd5, &IRQEnable, sizeof(IRQEnable));
-		//usleep(20);
+		IRQEnable = 1; 
+		write(fd5, &IRQEnable, sizeof(IRQEnable));
 		read(fd, &buf, 2);
 		AXI_TO_AUDIO_REG_0 = (int)buf;
-		//IRQEnable = 1; 
-		//write(fd5, &IRQEnable, sizeof(IRQEnable));
-		//printf("lol\n");
 	}
 }
